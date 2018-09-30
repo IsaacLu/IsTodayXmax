bc75b5ef5f142d3955f87b0de1d5c6e7d90447a1
Marcus Peng <ms855187@gmail.com>
2018-10-01 02:11:56 +0800

    Initial

---
+++ IsTodayXmas/Holiday.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IsTodayXmas
+{
+    public class Holiday
+    {
+        public string SayXmas()
+        {
+            var today = DateTime.Today;
+            if (today.Month == 12 && (today.Day == 24 || today.Day == 25))
+            {
+                return "Merry Xmas";
+            }
+
+            return "Today is not Xmas";
+        }
+    }
+}
---
+++ IsTodayXmasTests/HolidayTests.cs
@@ -0,0 +1,23 @@
+﻿using IsTodayXmas;
+using NUnit.Framework;
+
+namespace IsTodayXmasTests
+{
+    [TestFixture]
+    public class HolidayTests
+    {
+        [Test]
+        public void SayXmasTest_today_is_xmas()
+        {
+            var holiday = new Holiday();
+            Assert.AreEqual("Merry Xmas", holiday.SayXmas());
+        }
+
+        [Test]
+        public void SayXmasTest_today_is_not_xmas()
+        {
+            var holiday = new Holiday();
+            Assert.AreEqual("Today is not Xmas", holiday.SayXmas());
+        }
+    }
+}
